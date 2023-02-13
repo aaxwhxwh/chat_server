d7346347c2c0882520063eb4fde9cc6ff3f27561
熊文豪 <xiongwenhao@sinohealth.cn>
2023-02-13 11:29:56 +0800

    feat: demo project

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 
 app = Flask(__name__)
@@ -6,4 +6,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/work/callback", methods=["POST"])
+def work_callback():
+    print(request.content)
     return "<p>Hello, World!</p>"
